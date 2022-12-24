67cf954d2bfb3e5603e4d5a48d4cc97bcf02bc2c
Trqlli <104008218+Trqlli@users.noreply.github.com>
2022-12-24 13:01:25 +0100

    Update challanges.py

--- challanges.py
+++ challanges.py
@@ -9,11 +9,33 @@
 # [31, 123, 420, 69, 34, 89, 105]
 # [90, 2123, 3435]
 
+# Code:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Hint: Boolean = True or False
 
 # 2:
 
 # Make a function that counts to a given number
+
+#Code:
+
+
+
 
 # Hint: for loop
 
@@ -22,6 +44,14 @@
 
 # 1:
 # Make a while loop inside a function that prints the time (H, M, S) every second
+
+# Code:
+
+
+
+
+
+
 
 # Hint: When a while function is set to true it executes until its interrupted
 # Use a function from the time module to make the cooldown (you need to import it)
@@ -33,6 +63,14 @@
 # Names:
 # ["Alex", "Linkx", "Trolli", "Alec"]
 
+# Code:
+
+
+
+
+
+
+
 # Hint: .startswith()
 
 # Hard:
@@ -41,6 +79,18 @@
 # Make a function that requires the parameters name and age. Make two input fields that ask the name and age.
 # There cannot be numbers in the name and there cannot be letters in the age.
 
+# Code:
+
+
+
+
+
+
+
+
+
+
+
 # Hint: You will need any() and map() combined for checking the str. You can also use a for loop
 # You can use .isalpha to check if a str has numbers and .isdigit to check if a string has digits in it
 
@@ -48,122 +98,3 @@
 # map(): Makes function apply to each item in str, list... if item is iterable
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Easy Solution
-# 1:
-def check_list_bool_exercise(list) -> bool:
-    if len(list) < 6:
-        return True
-    else:
-        return False
-
-
-def list_check(list):
-    if check_list_bool_exercise(list):
-        print("The list is under the limit")
-    else:
-        print("The list is over the limit")
-
-
-list_check([31, 123, 420, 69, 34, 89, 105])
-list_check([90, 2123, 3435])
-
-
-# 2:
-def count(number):
-    for i in range(number):
-        i += 1
-        print(i)
-
-
-# Intermediate Solution
-# 1:
-import time
-
-
-def print_time_loop():
-    while True:
-        print(time.strftime("%H:%M:%S"))
-        time.sleep(1)
-
-
-print_time_loop()
-
-
-# 2:
-def check_name_start_letter(names):
-    count = 0
-    for i in names:
-
-        if i.startswith("A"):
-            count += 1
-    print(f"{count} people's names start with A")
-
-
-# Hard Solution
-def name_age_check():
-    name = input("What is your name? ")
-
-    if any(map(str.isdigit, name)):
-        print("You can't have numbers in your name!")
-    else:
-        age = input("What is your age? ")
-        if any(map(str.isalpha, age)):
-            print("You can't have letters in your age!")
-        else:
-            print(f"Your name is {name.title()} and you are {age} years old")
-
-
-name_age_check()
-
-
-# With for loop
-def name_age_check():
-    name = input("What is your name? ")
-
-    if any(i.isdigit() for i in name):
-        print("You can't have numbers in your name!")
-    else:
-        age = input("What is your age? ")
-        if any(j.isalpha() for j in age):
-            print("You can't have letters in your age!")
-        else:
-            print(f"Your name is {name.title()} and you are {age} years old")
-
-
-name_age_check()
