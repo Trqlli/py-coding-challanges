fec6f6df13e88b20caf2f07f051694acb2556e7d
Trqlli <104008218+Trqlli@users.noreply.github.com>
2022-12-30 19:20:30 +0100

    Create help.py

---
+++ help.py
@@ -0,0 +1,50 @@
+
+# Make a variable
+your_variable = str or int
+
+# Add to an int
+your_int = 0
+your_int += 1  # Added 1
+
+# Make a list/array
+your_list = [1, "2", 3, "4"]
+
+# Make a dictionary
+your_dictionary = {"item": "value"}
+
+# Make a tuple
+your_tuple = ("item1", "item2", "item3")
+
+# Get user input
+user_input = input()
+
+
+# Make a function
+def your_function():
+    # Your code...
+    pass
+
+
+# Reverse a string
+your_string = "Hello World"
+reversed_string = your_string[::-1]
+
+# or
+reverse = "".join(list(reversed("Reverse")))
+
+
+# Check if a string startswith something
+name = "Manuel"
+if name.startswith("M"):
+    # Your code
+    pass
+
+# Make a for loop
+for i in range(1):
+    # Your code...
+    pass
+
+# Make a while loop
+while True:
+    # Your code...
+    pass
