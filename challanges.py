219429d7e6370ec5b92cd720306a109af86fc1f0
Trqlli <104008218+Trqlli@users.noreply.github.com>
2023-01-20 23:12:54 +0100

    Update challanges.py

--- challanges.py
+++ challanges.py
@@ -11,6 +11,7 @@ The lists:
 [90, 2123, 3435]
 
 Code:
+"""
 
 
 
@@ -25,8 +26,7 @@ Code:
 
 
 
-
-
+"""
 Hint: Boolean = True or False
 
 2:
@@ -46,13 +46,13 @@ Intermediate:
 Make a while loop inside a function that prints the time (H, M, S) every second
 
 Code:
+"""
 
 
 
 
 
-
-
+"""
 Hint: When a while loop is set to true it executes until its interrupted
 Use a function from the time module to make the cooldown (you need to import it)
 Try using the string-formatted time function
@@ -64,13 +64,13 @@ Names:
 ["Alex", "Trolli", "Linx1337", "Alec"]
 
 Code:
+"""
 
 
 
 
 
-
-
+"""
 Hint: .startswith()
 
 
@@ -82,6 +82,7 @@ There cannot be numbers in the name and there cannot be letters in the age.
 Print the name and age in a sentence
 
 Code:
+"""
 
 
 
@@ -90,9 +91,7 @@ Code:
 
 
 
-
-
-
+"""
 Hint: You will need any() and map() combined for checking the str. You can also use a for loop
 You can use .isalpha to check if a str has numbers and .isdigit to check if a string has digits in it
 
